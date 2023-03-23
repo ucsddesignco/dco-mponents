0a9bea0eb657e00c4fb0abf0dde5fef3b3a32330
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-03-22 23:35:28 -0700

    Feat: Add css, jest testing

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
+import terser from '@rollup/plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 const packageJson = require('./package.json')
 
@@ -20,11 +23,12 @@ export default [
 				sourcemap: true,
 			},
 		],
-		plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+		plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss(), terser()],
 	},
 	{
 		input: 'dist/esm/types/index.d.ts',
 		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
 		plugins: [dts.default()],
+		external: [/\.css$/],
 	},
 ]
