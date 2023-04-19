d6b8b41b966e6eaa5f187048ba1317db2700cca8
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-19 02:01:01 -0700

    Feat: update ThumbnailList

--- src/components/ThumbnailList/ThumbnailList.tsx
+++ src/components/ThumbnailList/ThumbnailList.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uuid from 'react-uuid'
 
 import './ThumbnailList.scss'
 
+type DCoThumbnail = {
+	caption: string
+	src: string
+	alt: string
+	href?: string
+}
+
 interface ThumbnailListProps {
-	thumbnails: any
+	thumbnails: DCoThumbnail[]
 	thumbnailsPerRow?: number
 	forceSquare?: boolean
 	justifyContent?: string
 	imagePadding?: number
 }
 
-function ThumbnailList({ thumbnails, thumbnailsPerRow = 3, forceSquare = true, justifyContent = 'space-around', imagePadding = 0 }: ThumbnailListProps) {
+function ThumbnailList({ thumbnails, thumbnailsPerRow = 4, forceSquare = true, justifyContent = 'space-around', imagePadding = 0 }: ThumbnailListProps) {
+	const generateKeys = () => {
+		const thumbnailKeys: { [key:string] : string } = {}
+
+		thumbnails.forEach(thumbnail => {
+			thumbnailKeys[thumbnail.caption] = uuid()
+		})
+
+		return thumbnailKeys
+	}
+
+	// eslint-disable-next-line
+	const [uuidKeys, _] = useState(generateKeys())
+
 	return (
 		<div
 			className='dcomp-thumbnail-list-container'
@@ -18,27 +39,38 @@ function ThumbnailList({ thumbnails, thumbnailsPerRow = 3, forceSquare = true, j
 				justifyContent: justifyContent,
 			}}
 		>
-			{Object.keys(thumbnails).map(cap => (
-				<figure
-					className='dcomp-thumbnail-list-thumbnail'
-					style={
-						{
-							'--max-thumbnail-width': `calc(${Math.floor((100 / thumbnailsPerRow) * 100) / 100}%`,
+			{thumbnails.map(thumbnail => (
+				React.createElement(
+					// element name
+					thumbnail?.href !== undefined ? 'a' : 'figure',
+					// props
+					{
+						className: 'dcomp-thumbnail-list-thumbnail',
+						href: thumbnail?.href !== undefined ? thumbnail.href : undefined,
+						target: thumbnail?.href !== undefined ? '_blank' : undefined,
+						rel: thumbnail?.href !== undefined ? 'noreferrer' : undefined,
+						style: {
+							'--max-thumbnail-width': `${Math.floor((100 / thumbnailsPerRow) * 100) / 100}%`,
 							'--image-padding': `${imagePadding}px`,
-						} as React.CSSProperties
-					}
-				>
-					<img
-						className={'dcomp-thumbnail-list-image' + String(forceSquare && ' dcomp-thumbnail-list-image-square')}
-						src={thumbnails[cap]}
-						alt={`Some regularized description of ${cap}.`}
-						width='100%'
-					/>
-					<figcaption className='dcomp-thumbnail-list-image-caption'>{cap}</figcaption>
-				</figure>
+						},
+						key: uuidKeys[thumbnail.caption],
+					},
+					// children
+					<>
+						<img
+							className={'dcomp-thumbnail-list-image' + String(forceSquare && ' dcomp-thumbnail-list-image-square')}
+							src={thumbnail.src}
+							alt={thumbnail.alt}
+							width='100%'
+						/>
+						<figcaption className='dcomp-thumbnail-list-image-caption'>{thumbnail.caption}</figcaption>
+					</>,
+				)
 			))}
 		</div>
 	)
 }
 
 export default ThumbnailList
+
+export type { DCoThumbnail }
