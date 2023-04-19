c108a50123a6538d70e6f4cfe862a96a14747ec8
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-19 02:22:02 -0700

    Fix: Label component rollup errors

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,39 +1,34 @@
-import React from "react";
-import "./Label.scss";
+import React, { useEffect, useState } from 'react'
+import './Label.scss'
 
 export interface LabelProps {
-  /**  Sets the size*/
-  size?: "medium" | "small";
-  /**  Sets the text color */
-  foregroundColor?: string;
-  /**  Sets the background color */
-  backgroundColor?: string;
-  /**  Sets the label text */
-  children: string;
+	/**  Sets the size*/
+	size?: 'medium' | 'small'
+	/**  Sets the text color */
+	foregroundColor?: string
+	/**  Sets the background color */
+	backgroundColor?: string
+	/**  Sets the label text */
+	children: any
 }
 
-const Label = ({
-  size = "medium",
-  foregroundColor = "#ffffff",
-  backgroundColor = "#000000",
-  children,
-}: LabelProps) => {
-  const padding = size === "medium" ? 12 : 8;
-  const fontSize = size === "medium" ? 16 : 12;
+function Label({ size = 'medium', foregroundColor = '#ffffff', backgroundColor = '#000000', children }: LabelProps) {
+	const [padding, setPadding] = useState<number>(0)
+	const [fontSize, setFontSize] = useState<number>(0)
 
-  return (
-    <div
-      className="Label"
-      style={{
-        color: foregroundColor,
-        background: backgroundColor,
-        padding,
-        fontSize,
-      }}
-    >
-      {children}
-    </div>
-  );
-};
+	useEffect(() => {
+		setPadding(size === 'medium' ? 12 : 8)
+		setFontSize(size === 'medium' ? 16 : 12)
+	}, [size])
 
-export default Label;
+	return (
+		<div
+			className='dcomp-test-component-Label'
+			style={{ color: foregroundColor, backgroundColor: backgroundColor, padding: padding, fontSize: fontSize }}
+		>
+			{children}
+		</div>
+	)
+}
+
+export default Label
