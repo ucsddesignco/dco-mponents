3159e9ee1450d18b7349c6d98f540c0c77e466c1
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-19 16:13:36 -0700

    Updates

---
+++ sb-addons/storybook-9/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var p=__STORYBOOKADDONS__,{addons:n,types:d,mockChannel:l}=__STORYBOOKADDONS__;n.setConfig({showPanel:!0,panelPosition:"right"});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
