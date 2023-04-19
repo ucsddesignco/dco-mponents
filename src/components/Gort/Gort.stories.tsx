8eb07a5f7a4cdc736078e677098ee5a3675ae35c
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-04-19 02:03:21 -0700

    Trivial: Gort stories typing

--- src/components/Gort/Gort.stories.tsx
+++ src/components/Gort/Gort.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { ComponentStory, ComponentMeta } from '@storybook/react'
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
 import Gort from './Gort'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -14,7 +13,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Gort> = args => <Gort {...args} />
+const Template: StoryFn = args => <Gort {...args} />
 
 export const Autoplay: Story = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
