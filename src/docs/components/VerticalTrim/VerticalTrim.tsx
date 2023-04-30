6a60122d62212c0977aefff282e405d887dfccd7
gambowoa <baymarket96@gmail.com>
2023-04-29 17:19:19 -0700

    add vertical trim component to Typography docs

---
+++ src/docs/components/VerticalTrim/VerticalTrim.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './VerticalTrim.scss';
+import Placeholder from '../../../components/Placeholder/Placeholder';
+
+const VerticalTrim = () => {
+  return (
+    <div id="dc-docs-vertical-trim">
+      <Placeholder />
+      <span className="neue-plak">VerticalTrim</span>
+      <Placeholder />
+      <span className="nb-international">VerticalTrim</span>
+      <Placeholder />
+    </div>
+  );
+};
+
+export default VerticalTrim;
